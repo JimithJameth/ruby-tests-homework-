d03da8b7c09ac5b7d94bda38727037b7c80cb64d
Jamie Martin <jamiemartinmusic@gmail.com>
2017-05-23 17:42:52 +0100

    tenth test pass

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -42,3 +42,16 @@ def number_to_full_month_name(int)
     return "September"
   end
 end
+
+def number_to_short_month_name(int)
+  case int
+  when 1
+    return "Jan"
+
+  when 3
+    return "Mar"
+
+  when 9
+    return "Sep"
+  end
+end
